1879ab53aaddcbaac2f97375334747fb652292ab
JesusKid <frawllings@gmail.com>
2021-10-16 11:49:15 +0100

    forms password area changed

--- forms.py
+++ forms.py
@@ -20,7 +20,7 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField('Login')
 
 class CommentForm(FlaskForm):
